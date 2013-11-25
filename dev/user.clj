fbb251ef30badc19eb85a9af7e22e8c0f6d7629a
Simon Katz <sk@nomistech.com>
2013-11-25 15:55:53 +0000

    Revert back to some of Stuart Sierra's naming, so that this is in line with anyone else who's using Stuart's ideas: - `init` instead of `create` - `go` instead of `create-and-start`.

--- dev/user.clj
+++ dev/user.clj
@@ -18,14 +18,14 @@
 (defonce the-system
   ;; "A container for the current instance of the application.
   ;; Only used for interactive development."
-  ;;
+  ;; 
   ;; Don't want to lose this value if this file is recompiled (when
   ;; changes are made to the useful additional utilities for the REPL
   ;; at the end of the file), so use `defonce`.
   ;; But note that this /is/ blatted when a `reset` is done.
   nil)
 
-(defn create
+(defn init
   "Creates a system and makes it the current development system."
   []
   (alter-var-root #'the-system
@@ -42,16 +42,16 @@
   (alter-var-root #'the-system
     (fn [s] (when s (system/stop s)))))
 
-(defn create-and-start
+(defn go
   "Creates a system, makes it the current development system and starts it."
   []
-  (create)
+  (init)
   (start))
 
 (defn reset []
-  "Stop, refresh and create-and-start."
+  "Stop, refresh and go."
   (stop)
-  (refresh :after 'user/create-and-start))
+  (refresh :after 'user/go))
 
 ;;;; Useful additional utilities for the REPL
 
